f97f2573e8b1703228f72b7ffc12be539300878d
mrAppleXZ <mr.applexz@gmail.com>
2016-07-30 14:55:03 +0500

    Bump version

--- Botico/BoticoClient.cs
+++ Botico/BoticoClient.cs
@@ -13,7 +13,7 @@ namespace Botico
 		public static string Path => AppDomain.CurrentDomain.BaseDirectory + "/Botico/";
 		public static string PathLangs => Path + "langs/";
 		public static string PathConfig => Path + "config.json";
-		public static string Version => "0.0.2";
+		public static string Version => "0.2.0";
 
 		public char? CommandSymbol { get; set; }
 		public string ClientName { get; set; }
